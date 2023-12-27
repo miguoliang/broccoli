e46ff8e0e3dbacdfe772051518aaa9c9d41d30d3
Mi Guoliang <boymgl@qq.com>
2023-12-27 13:21:10 +0800

    fix: compile error

--- src/main/java/broccoli/models/EdgeId.java
+++ src/main/java/broccoli/models/EdgeId.java
@@ -6,5 +6,6 @@ import java.io.Serializable;
  * The {@link EdgeId}.
  */
 
-record EdgeId(int inVertex, int outVertex, String name, String scope) implements Serializable {
+public record EdgeId(int inVertex, int outVertex, String name, String scope)
+    implements Serializable {
 }
