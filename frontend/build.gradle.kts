1cb1ae3d9e8f6587281cedb0d5e66e0d8bc88fb5
Mi Guoliang <boymgl@qq.com>
2024-01-13 15:23:41 +0800

    update: github workflow

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -4,6 +4,14 @@ plugins {
     id("com.github.node-gradle.node") version "7.0.1"
 }
 
+node {
+    version = "20.10.0"
+    npmVersion = "10.2.3"
+    download = true
+    workDir = file(".gradle/node")
+    pnpmWorkDir = file(".gradle/pnpm")
+}
+
 tasks.register<PnpmTask>("build") {
     group = "build"
     description = "Builds the frontend"
@@ -11,4 +19,15 @@ tasks.register<PnpmTask>("build") {
     args.set(arrayListOf("run", "build"))
 
     dependsOn("pnpmInstall")
+}
+
+tasks.register("clean") {
+    group = "build"
+    description = "Cleans the frontend"
+
+    doLast {
+        file("build").deleteRecursively()
+        file("node_modules").deleteRecursively()
+        file("index.css").delete()
+    }
 }
